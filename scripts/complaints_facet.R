b71bcb08deb6c52cf8dc85f76b7ab0b70bfd841c
Apoorv Anand <5118689+apoorv74@users.noreply.github.com>
2023-08-28 18:58:02 +0530

    update facet graphs script

--- scripts/complaints_facet.R
+++ scripts/complaints_facet.R
@@ -1,64 +1,76 @@
 source("scripts/libraries.R")
 source("scripts/vars.R")
 
-raw_data <- read_csv("data/21082023/raw_data_sheet.csv")
+processed_data_path <-
+  glue::glue("data/{data_shared_on}/appended_data_sheet.csv")
+
+data_for_viz <- read_csv(processed_data_path)
+
+# Filter data for selected categories
+data_for_viz_sub <- data_for_viz[data_for_viz$new_category %in% c("Drainage","Roads","Sanitation","Water Supply"),]
 
 vars_to_use <-
-  c("re_re_categ", "Zone", "Problem.Category", "Problem", "week")
+  c("new_category", "zone", "problem_category", "problem", "week")
 
-data <- raw_data[,vars_to_use]
-data <- left_join(data, problem_df, by="Problem")
-data <- data[!is.na(data$Problem_recat),]
-data$week <-
-  stringr::str_replace_all(data$week, pattern = "_Week", replacement = "")
+data <- data_for_viz_sub[,vars_to_use]
 
-categories_for_viz <- data$re_re_categ %>% unique()
+data <- left_join(data, problem_df, by="problem")
+
+
+data <- data[!is.na(data$problem_recat),]
+
+# data$week <-
+#   stringr::str_replace_all(data$week, pattern = "_Week", replacement = "")
+
+categories_for_viz <- data$new_category %>% unique()
+
 for(i in 1:length(categories_for_viz)){
   
   cat_i <- categories_for_viz[[i]]
   
-  cat_df <- data[data$re_re_categ == cat_i,]
+  cat_df <- data[data$new_category == cat_i,]
   
   viz_data <-
     cat_df %>% 
-    group_by(Zone, Problem_recat, week) %>% 
+    group_by(zone, problem_recat, week) %>% 
     summarise(complaints = n()) %>% data.frame(check.names = FALSE)
   
   # Only consider top 4 complaints
   viz_data_filter <-
     viz_data %>%
-    group_by(Problem_recat) %>%
+    group_by(problem_recat) %>%
     summarise(total = sum(complaints)) %>%
     arrange(desc(total)) %>%
     head(4) %>%
-    pull(Problem_recat)
+    pull(problem_recat)
   
   viz_data <-
-    viz_data[viz_data$Problem_recat %in% viz_data_filter, ]
+    viz_data[viz_data$problem_recat %in% viz_data_filter, ]
   
   problem_order <- viz_data %>%
-    group_by(Problem_recat) %>%
+    group_by(problem_recat) %>%
     summarize(total_complaints = sum(complaints)) %>%
     arrange(total_complaints) %>%
-    pull(Problem_recat)
+    pull(problem_recat)
   
-  viz_data$Problem_recat <-
-    factor(viz_data$Problem_recat, levels = problem_order)
+  viz_data$problem_recat <-
+    factor(viz_data$problem_recat, levels = problem_order)
   
-  custom_colors <- c("Current" = "#BAE2BB", "Previous" = "#DEC280")
+  custom_colors <- c("current" = "#BAE2BB", "previous" = "#DEC280")
   
   
-  bar_viz <- ggplot(viz_data, aes(x = Problem_recat, y = complaints, fill = week)) +
+  bar_viz <- ggplot(viz_data, aes(x = problem_recat, y = complaints, fill = week)) +
     geom_bar(stat = "identity", position = "dodge") +
     labs(x = "Problem Category", y = "Number of Complaints per week", fill = "Week") +
     scale_fill_manual(values = custom_colors) +  # Set custom colors
     theme_minimal() + 
     coord_flip() + 
-    facet_wrap(~ Zone, ncol = 3) + 
+    facet_wrap(~ zone, ncol = 3) + 
     guides(fill = guide_legend(reverse = TRUE))
   
+    viz_path <- glue::glue("viz/{data_shared_on}/weekly-complaints-bar")
     ggsave(plot = bar_viz, filename = glue::glue("{cat_i}.png"), 
-           path = "viz/220823/weekly-complaints-bar/",bg = "white")
+           path = viz_path,bg = "white")
   
 }
 
--- scripts/vars.R
+++ scripts/vars.R
@@ -48,7 +48,7 @@ ward_numbers$ward <-
 
 problem_df <- data.frame(
   stringsAsFactors = FALSE,
-           Problem = c("Drainage- Choking Of Line",
+           problem = c("Drainage- Choking Of Line",
                        "Drainage- Manhole Cover Missing","Drainage-Other",
                        "Drainage-Public Toilets and Urinals - Drainage Line Blockage or Choking","Water-No Supply",
                        "Water-Leakage In Main Line","Water-low  pressure","Water-Pollution In Supply",
@@ -62,14 +62,16 @@ problem_df <- data.frame(
                        "SWM-Clearing off the Dead Animals",
                        "Using Inferior Quality of Plastic for Tea/Pan/Water pouch/Other Food Throwing Plastic garbage/Waste",
                        "SWM-Clearing off the Big Dead Animals","Smart Toilet-Automatic Door is not working",
-                       "Smart Toilet-Non Availability of water"),
-     Problem_recat = c("Choking","Manhole cover",
+                       "Smart Toilet-Non Availability of water",
+                       "Removal of waste-Dust Lying on both side of the road after re surfacing","Smart Toilet-Daily Cleaning not being done"),
+     problem_recat = c("Choking","Manhole cover",
                        "Other","Public Toilet","No supply","Leakage",
                        "Low pressure","Pollution","Other","Bhuva","Other","Repair",
                        "D2D vehicle","Cleaning","Public toilets - Surroundings",
                        "Waste Burning","Spitting/Urinating",
                        "Debris not cleared","Public toilets - Cleaning",
                        "Clearing dead animals","Plastic waste","Clearing big dead animals",
-                       "Smart toilet - automatic door",
-                       "Smart toilet - water availability")
+                       "Smart toilet - Automatic Door",
+                       "Smart toilet - Water Availability",
+                       "Dust Removal","Smart Toilet - Daily Cleaning")
 )
